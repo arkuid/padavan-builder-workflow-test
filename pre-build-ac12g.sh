6dff4a2d9ca620101d7789a8cfff02a2fd0075eb
arkuid <149357139+arkuid@users.noreply.github.com>
2026-02-11 11:32:51 +0500

    Update and rename pre-build.sh to pre-build-ac12g.sh

---
+++ pre-build-ac12g.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ls -la
+ZAPRET_VERSION="72.9"
+NFQWS_DIR="padavan-ng/trunk/user/nfqws"
+echo "Cleaning old zapret files..."
+rm -rf "$NFQWS_DIR"/zapret-* "$NFQWS_DIR"/zapretsh-main
+rm -f "$NFQWS_DIR"/*.tar.gz
+echo "✓ Old zapret files cleaned"
+CURRENT_VERSION=$(grep -E '^SRC_VER\s*=\s*[0-9.]+' "$NFQWS_DIR/Makefile" | head -1 | awk -F'=' '{print $2}' | tr -d ' ')
+[[ -z "$CURRENT_VERSION" ]] && echo "Cannot detect zapret version" && exit 1
+echo "Updating zapret from $CURRENT_VERSION to $ZAPRET_VERSION"
+sed -i "s/^SRC_VER = $CURRENT_VERSION/SRC_VER = $ZAPRET_VERSION/" "$NFQWS_DIR/Makefile"
+grep -q "^SRC_VER = $ZAPRET_VERSION" "$NFQWS_DIR/Makefile" && echo "✓ Zapret version updated" || (echo "Update failed" && exit 1)
+echo "Done."
+
+
+RTL8367_DIR="padavan-ng/trunk/linux-3.4.x/drivers/net/rtl8367"
+NET_DIR="padavan-ng/trunk/linux-3.4.x/drivers/net/"
+echo "Modifed rtl8367..."
+rm -r "$RTL8367_DIR"/rtl8367_drv.c "$RTL8367_DIR"/Makefile "$RTL8367_DIR"/Kconfig
+tar -xf "$ROUTER_MODEL"/rtl8367.tar.gz -C "$NET_DIR"
+ls -la "$RTL8367_DIR"
