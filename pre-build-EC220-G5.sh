126e95ca5fe0a68fb7d45e90aa2ba291389ebefd
arkuid <149357139+arkuid@users.noreply.github.com>
2026-02-18 00:49:18 +0500

    Update pre-build-EC220-G5.sh

--- pre-build-EC220-G5.sh
+++ pre-build-EC220-G5.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ls -la
+
+NFQWS_DIR="padavan-ng-test/trunk/user/nfqws"
+echo "Cleaning old zapret files..."
+rm -rf "$NFQWS_DIR"/zapret-* "$NFQWS_DIR"/zapretsh-main
+rm -f "$NFQWS_DIR"/*.tar.gz
 #RTL8367_DIR="padavan-ng/trunk/linux-3.4.x/drivers/net/rtl8367"
 #echo "Cleaning old zapret files..."
 #rm -r "$RTL8367_DIR"/rtl8367_drv.c "$RTL8367_DIR"/Makefile "$RTL8367_DIR"/Kconfig
